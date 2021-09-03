75f2b25eaac11d154882a8a598d9b24b3b40875f
Gavin Sweeney <gavswe19@gmail.com>
2021-09-03 19:55:38 -0400

    Add Season Type full names

--- src/Components/Leaderboards/LeaderboardOptions/SeasonType.js
+++ src/Components/Leaderboards/LeaderboardOptions/SeasonType.js
@@ -3,6 +3,12 @@ import React from 'react'
 export function SeasonType(props) {
     const seasonTypesOptions = ["PRE", "REG", "POST"];
 
+    const seasonTypeDisplay = {
+        "PRE": "Preseason",
+        "REG": "Regular Season",
+        "POST": "Postseason"
+    }
+
     return (
         <div className="w-full mt-3 lg:mt-0 lg:w-1/4 px-5 text-center">
             <div className="border-b-2 border-gray-100 font-bold">
@@ -12,7 +18,7 @@ export function SeasonType(props) {
             <div>
                 {seasonTypesOptions.map(st => (
                     <div key={st} className={`w-40 mx-auto m-1 border-2 rounded-md cursor-pointer hover:bg-gray-100 ${props.seasonType.includes(st) ? "border-green-400" : "border-white"}`} onClick={() => seasonTypeClick(st)}>
-                        {st}
+                        {seasonTypeDisplay[st]}
                     </div>
                 ))}
             </div>
