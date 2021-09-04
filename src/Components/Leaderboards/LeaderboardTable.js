b32da31fd1f668836c36dcb3b344e60898de8635
Gavin Sweeney <gavswe19@gmail.com>
2021-09-04 01:47:14 -0400

    Update leaderboard style

--- src/Components/Leaderboards/LeaderboardTable.js
+++ src/Components/Leaderboards/LeaderboardTable.js
@@ -5,26 +5,32 @@ import { LeaderboardTableRow } from './LeaderboardTableRow';
 export function LeaderboardTable(props) {
     if (props.loading) {
         return (
-            <div className="w-full sm:min-h-screen lg:min-h-0 mx-auto mt-5 bg-white rounded-md border border-gray-300 flex-1 overflow-auto text-xs lg:text-sm font-mono font-light text-center">
-                <div className="mt-5">
-                    <img className="w-14 h-14 mx-auto animate-bounce" src="https://static.www.nfl.com/image/upload/v1554321393/league/nvfr7ogywskqrfaiu38m.svg" />
+            <>
+                <div className="mt-5 text-sm text-gray-700 uppercase text-center">{props.type} Leaders</div>
+                <div className="w-full sm:min-h-screen lg:min-h-0 mx-auto bg-white rounded-md border border-gray-300 flex-1 overflow-auto text-xs lg:text-sm font-mono font-light text-center">
+                    <div className="mt-5">
+                        <img className="w-14 h-14 mx-auto animate-bounce" src="https://static.www.nfl.com/image/upload/v1554321393/league/nvfr7ogywskqrfaiu38m.svg" />
+                    </div>
+                    <div className="text-gray-700 text-sm uppercase font-light">
+                        Loading
+                    </div>
                 </div>
-                <div className="text-gray-700 text-sm uppercase font-light">
-                    Loading
-                </div>
-            </div>
+            </>
         )
     }
     else if (props.playerArray.length === 0) {
         return (
-            <div className="w-full sm:min-h-screen lg:min-h-0 mx-auto mt-5 bg-white rounded-md border border-gray-300 flex-1 overflow-auto text-xs lg:text-sm font-mono font-light text-center">
-                <div className="mt-5">
-                    <img className="w-14 h-14 mx-auto animate-bounce" src="https://static.www.nfl.com/image/upload/v1554321393/league/nvfr7ogywskqrfaiu38m.svg" />
+            <>
+                <div className="mt-5 text-sm text-gray-700 uppercase text-center">{props.type} Leaders</div>
+                <div className="w-full sm:min-h-screen lg:min-h-0 mx-auto bg-white rounded-md border border-gray-300 flex-1 overflow-auto text-xs lg:text-sm font-mono font-light text-center">
+                    <div className="mt-5">
+                        <img className="w-14 h-14 mx-auto animate-bounce" src="https://static.www.nfl.com/image/upload/v1554321393/league/nvfr7ogywskqrfaiu38m.svg" />
+                    </div>
+                    <div className="text-gray-700 text-sm uppercase font-light">
+                        No Results for Selection
+                    </div>
                 </div>
-                <div className="text-gray-700 text-sm uppercase font-light">
-                    No Results for Selection
-                </div>
-            </div>
+            </>
         )
     }
     else {
